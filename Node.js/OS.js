4d8034789dff1a9adb8c18f1bcdc74f3cde1cad1
rosindubh <p.welsby2@ntlworld.com>
2021-08-22 08:56:31 +0100

    added some comments

--- Node.js/OS.js
+++ Node.js/OS.js
@@ -1,5 +1,18 @@
-// phil welsby - 21 aug 2021 - practice using OS Module
+// phil welsby 21 aug - 2021
+
+//The os module provides operating system-related utility methods and properties.
+//It can be accessed using:
 
 const os = require('os');
-const current = os.homedir();
-console.log('current working directory is:', current);
+
+//console.log(os.arch());
+
+//Returns an array of objects containing information about each logical CPU core.
+console.log(os.cpus());
+
+console.log(os.homedir()); //displays home dir
+console.log(os.hostname()); //displays hostname of computer
+console.log(os.loadavg());
+console.log(os.networkInterfaces()); // displays network interfaces
+console.log(os.uptime()); // displays uptime in seconds
+
---
+++ Node.js/show-working-directory.js
@@ -0,0 +1,5 @@
+// phil welsby - 21 aug 2021 - practice using OS Module
+
+const os = require('os');
+const current = os.homedir();
+console.log('current working directory is:', current,);
