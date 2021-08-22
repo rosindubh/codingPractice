b0f7709fc13339c775a16ec17b0a488db5789388
rosindubh <p.welsby2@ntlworld.com>
2021-08-22 21:23:15 +0100

    put switch into a for loop

--- just-for-fun/test.js
+++ just-for-fun/test.js
@@ -2,5 +2,30 @@
 
 const numbers = [1, 2, 3, 4, 5];
 
+// higher level of abstraction using filter
 const oddArray = numbers.filter((number) => number % 2 !== 0);
 console.log(oddArray);
+
+// simple if/else statement
+let age = 18;
+let country = 'UK';
+if (age >= 18 && country == 'UK') {
+  console.log("Yes I can serve you");
+} else {
+  console.log("You're not old enough");
+}
+
+// example using a switch
+let toppings = ['mushrooms', 'pepperoni', 'anchovies', 'pineapple'];
+for (let i = 0; i < toppings.length; i++) {
+
+    switch(toppings[i]) {
+      case 'pepperoni': console.log(`${toppings[i]} is good`);
+        break;
+      case 'pineapple': console.log(`${toppings[i]} is bad`);
+        break;
+      case 'mushrooms': console.log(`${toppings[i]} is good`);
+        break;
+      case 'anchovies': console.log(`${toppings[i]} is well out of order`);
+    }
+}
