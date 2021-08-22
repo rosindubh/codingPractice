fbb16195f8c1d1d46e4f38439d2890e25a47827e
rosindubh <p.welsby2@ntlworld.com>
2021-08-22 18:30:32 +0100

    re-wrote [console.log] line and removed [const current]

--- Node.js/show-working-directory.js
+++ Node.js/show-working-directory.js
@@ -1,5 +1,5 @@
 // phil welsby - 21 aug 2021 - practice using OS Module
 
 const os = require('os');
-const current = os.homedir();
-console.log('current working directory is:', current,);
+
+console.log('current working directory is', os.homedir());
