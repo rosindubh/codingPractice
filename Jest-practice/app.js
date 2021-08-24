4847e7bd0b97a31526083ab92562515a7e3adac3
rosindubh <p.welsby2@ntlworld.com>
2021-08-24 08:54:23 +0100

    added an object and a test

--- Jest-practice/app.js
+++ Jest-practice/app.js
@@ -2,6 +2,14 @@ const add = (num1, num2) => {
   return num1 + num2
 }
 
+const theObject = {
+  singer: "Robert Plant",
+  guitar: "Jimmy Page",
+  bass: "John Paul Jones",
+  drums: "John Heny Bonham"
+}
+
 module.exports = {
-  add
+  add,
+  theObject
 }
--- Jest-practice/app.test.js
+++ Jest-practice/app.test.js
@@ -4,3 +4,6 @@ test("should return 5 when given 2 and 3", () => {
   expect(app.add(2, 3)).toBe(5)
 });
 
+test("should expect Jimmy Page when given theObject.guitar", () => {
+  expect(app.theObject.guitar).toBe("Jimmy Page")
+});
