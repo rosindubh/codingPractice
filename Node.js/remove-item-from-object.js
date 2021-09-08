da7b9bcebea22f48b2566154ce395d5abbe38a7a
rosindubh <p.welsby2@ntlworld.com>
2021-09-08 13:11:45 +0100

    incomplete

---
+++ Node.js/remove-item-from-object.js
@@ -0,0 +1,8 @@
+
+let object = [
+{"name":"BladeRunner","actor":"Harrison Ford","year":"1982","rating":"good"},
+{"name":"Highlander","actor":"Christopher Lambert","year":"1986","rating":"good"},
+{"name":"Breakfast Club Emilio","actor":"Estervez 1985 good"},
+{"name":"Pulp Fiction","actor":"Samual L Jackson","year":"1994","rating":"good"}
+]
+
